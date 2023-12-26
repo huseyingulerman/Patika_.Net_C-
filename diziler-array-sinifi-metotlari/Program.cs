9e0af2e9a67ae41af4d67107d704d1908fce8505
hüseyin gülerman <huseyingulerman.1997@gmail.com>
2023-12-26 15:19:06 +0300

    Array

---
+++ diziler-array-sinifi-metotlari/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace diziler_array_sinifi_metotlari
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

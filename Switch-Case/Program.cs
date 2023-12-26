4008f71bbfbdc93ed05a167cdca9492ab3d78ab1
hüseyin gülerman <huseyingulerman.1997@gmail.com>
2023-12-26 13:59:00 +0300

    Switch case

---
+++ Switch-Case/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Switch_Case
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

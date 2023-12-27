d52a03bab93e898066f696932d47612a27418d7c
hüseyin gülerman <huseyingulerman.1997@gmail.com>
2023-12-27 14:40:33 +0300

    generic list

---
+++ generic-list/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace generic_list
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
